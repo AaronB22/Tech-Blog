d8290631edfbd128d8386c9eb60ddebfb760b127
Aaron <aaronthesnowboarder@live.com>
2021-06-07 17:47:00 -0700

    setup for jaws.db

--- dist/server.js
+++ dist/server.js
@@ -4,12 +4,12 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helper');
+const helper = require('./utils/helper');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helper });
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
@@ -24,7 +24,8 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
-app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
-app.listen(PORT, () => console.log(`${PORT} is now Online`));
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`${PORT} online`));
+});
